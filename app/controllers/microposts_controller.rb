1e06eed907260d7aada86b0736448c517e1c071d
jdmartinez1062 <jd.martinez1062@gmail.com>
2020-03-31 14:41:11 -0500

    added micropost controller and implemented post creation

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,3 +1,18 @@
 class MicropostsController < ApplicationController
+  include ApplicationHelper
     belongs_to :user
+    before_action :logged_in_user, only: [:create, :destroy]
+    before_action :correct_user,   only: [:create, :destroy]
+
+    def create
+    end
+
+    def destroy
+    end
+
+    private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end
